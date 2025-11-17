31062ea0c15ac479eb5a66f4187ae77af75e5d53
Kaoutar laajil <129660727+kooutar@users.noreply.github.com>
2025-11-17 15:20:29 +0100

    chore : config de bdd avec .env

---
+++ src/main/java/com/kaoutar/Eventify/model/Event.java
@@ -0,0 +1,32 @@
+package com.kaoutar.Eventify.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Event {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    @Column(length = 1000)
+    private String description;
+
+    private String location;
+
+    private LocalDateTime dateTime;
+
+    private Integer capacity;
+
+    private Long organizerId;
+}
