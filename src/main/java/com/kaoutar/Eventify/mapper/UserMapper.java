ffda5de025849020e421f00f661610cc8a191e45
Asmae el hamzaoui <asmaeelhamzaoui77@gamil.com>
2025-11-17 16:16:10 +0100

    lfeat : la création du mapper de user

--- src/main/java/com/kaoutar/Eventify/mapper/UserMapper.java
+++ src/main/java/com/kaoutar/Eventify/mapper/UserMapper.java
@@ -1,4 +1,16 @@
 package com.kaoutar.Eventify.mapper;
 
+import com.kaoutar.Eventify.dto.UserDTO;
+import com.kaoutar.Eventify.model.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper(componentModel = "spring")
 public interface UserMapper {
+
+    UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
+
+    UserDTO toDTO(User user);
+
+    User toEntity(UserDTO userDTO);
 }
