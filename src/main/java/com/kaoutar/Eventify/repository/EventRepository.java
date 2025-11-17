19e1a65a0854cca71de9257e79ff4e9df69524fe
Kaoutar laajil <129660727+kooutar@users.noreply.github.com>
2025-11-17 17:03:03 +0100

    feat : creeation de EventRepository.java

--- src/main/java/com/kaoutar/Eventify/repository/EventRepository.java
+++ src/main/java/com/kaoutar/Eventify/repository/EventRepository.java
@@ -1,4 +1,10 @@
 package com.kaoutar.Eventify.repository;
 
-public class EventRepository {
+
+import com.kaoutar.Eventify.model.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EventRepository extends JpaRepository<Event, Long> {
+    // Si besoin, tu peux ajouter des méthodes personnalisées, par exemple :
+    // List<Event> findByOrganizerId(Long organizerId);
 }
