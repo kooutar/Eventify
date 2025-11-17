4ca860f97def2e5b6417ee362d1c51abcd1263c1
Asmae el hamzaoui <asmaeelhamzaoui77@gamil.com>
2025-11-17 16:20:24 +0100

    lfeat: ll'implémentation du ripository de l'entité User

--- src/main/java/com/kaoutar/Eventify/repository/UserRepository.java
+++ src/main/java/com/kaoutar/Eventify/repository/UserRepository.java
@@ -1,4 +1,17 @@
 package com.kaoutar.Eventify.repository;
 
-public interface UserRepository {
+import com.kaoutar.Eventify.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    // Chercher un utilisateur par email
+    Optional<User> findByEmail(String email);
+
+    // Vérifier si un email existe déjà
+    boolean existsByEmail(String email);
 }
