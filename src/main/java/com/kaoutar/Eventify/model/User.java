5eeb81393ded029abced90c43a6e76d3efadf19a
Asmae el hamzaoui <asmaeelhamzaoui77@gamil.com>
2025-11-17 16:06:38 +0100

    feat: la création de la classe user

--- src/main/java/com/kaoutar/Eventify/model/User.java
+++ src/main/java/com/kaoutar/Eventify/model/User.java
@@ -1,4 +1,30 @@
 package com.kaoutar.Eventify.model;
 
+import com.kaoutar.Eventify.enums.Role;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "user")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private Role role;
 }
