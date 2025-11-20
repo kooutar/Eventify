5618853c244dd05f0c7a60ac2d507d315df5df50
Asmae el hamzaoui <asmaeelhamzaoui77@gamil.com>
2025-11-20 15:23:35 +0100

    lfeat: la'implémentation de la classe JwtAuthentificationEntryPoint pour la gestion des erreurs 401 Unauthorized

--- src/main/java/com/kaoutar/Eventify/security/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/kaoutar/Eventify/security/JwtAuthenticationEntryPoint.java
@@ -1,4 +1,36 @@
 package com.kaoutar.Eventify.security;
 
-public class JwtAuthenticationEntryPoint {
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request,
+                         HttpServletResponse response,
+                         AuthenticationException authException) throws IOException {
+
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED); // 401
+        response.setContentType("application/json");
+
+        Map<String, Object> body = new HashMap<>();
+        body.put("timestamp", LocalDateTime.now().toString());
+        body.put("status", 401);
+        body.put("error", "Unauthorized");
+        body.put("message", "Authentication is required to access this resource");
+        body.put("path", request.getRequestURI());
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.writeValue(response.getOutputStream(), body);
+    }
 }
