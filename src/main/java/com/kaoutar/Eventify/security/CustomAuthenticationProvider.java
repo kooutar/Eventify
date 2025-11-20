43f13f80f80e7b9d8d031be5dff74947f8a1f3bf
Kaoutar laajil <129660727+kooutar@users.noreply.github.com>
2025-11-20 14:46:45 +0100

    feat : ajout authenticate et supports mathodes

--- src/main/java/com/kaoutar/Eventify/security/CustomAuthenticationProvider.java
+++ src/main/java/com/kaoutar/Eventify/security/CustomAuthenticationProvider.java
@@ -1,4 +1,33 @@
 package com.kaoutar.Eventify.security;
 
-public class CustomAuthenticationProvider {
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class CustomAuthenticationProvider implements AuthenticationProvider {
+    private  final CustomUserDetailsService userDetailsService;
+    private  final PasswordEncoder passwordEncoder;
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String username= authentication.getName();
+        String password = authentication.getCredentials().toString();
+
+        var userDetails = userDetailsService.loadUserByUsername(username);
+        if(!passwordEncoder.matches(password , userDetails.getPassword())){
+            throw new BadCredentialsException("Bad credentials ");
+        }
+        return new UsernamePasswordAuthenticationToken(userDetails,password,userDetails.getAuthorities());
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication);
+    }
 }
