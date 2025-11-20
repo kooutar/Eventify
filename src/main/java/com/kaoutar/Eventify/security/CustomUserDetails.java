80af44e203c6d8f8cc4c4c9b2941dabdecc1c6c9
Kaoutar laajil <129660727+kooutar@users.noreply.github.com>
2025-11-20 12:06:22 +0100

    feat : implementer les methodes des interfaces UserDetails

--- src/main/java/com/kaoutar/Eventify/security/CustomUserDetails.java
+++ src/main/java/com/kaoutar/Eventify/security/CustomUserDetails.java
@@ -1,4 +1,33 @@
 package com.kaoutar.Eventify.security;
 
-public class CustomUserDetails {
+import com.kaoutar.Eventify.model.User;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+@Getter
+public class CustomUserDetails implements UserDetails {
+    private  final User user;
+
+    public CustomUserDetails(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(user.getRole().name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
 }
