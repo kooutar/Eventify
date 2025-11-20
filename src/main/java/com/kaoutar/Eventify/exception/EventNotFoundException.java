ab8915ac14ab066af97b2290c0a4ca2a7230b2a8
Asmae el hamzaoui <asmaeelhamzaoui77@gamil.com>
2025-11-20 16:50:57 +0100

    feat: l'implémentation de l'exception EventNotFoundException

--- src/main/java/com/kaoutar/Eventify/exception/EventNotFoundException.java
+++ src/main/java/com/kaoutar/Eventify/exception/EventNotFoundException.java
@@ -1,4 +1,7 @@
 package com.kaoutar.Eventify.exception;
 
-public class EventNotFoundException {
+public class EventNotFoundException extends RuntimeException {
+    public EventNotFoundException(String message) {
+        super(message);
+    }
 }
