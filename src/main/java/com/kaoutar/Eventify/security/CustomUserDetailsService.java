b04be3f5641ef3a6cd0f4df586412e4ac36e95a7
Kaoutar laajil <129660727+kooutar@users.noreply.github.com>
2025-11-20 12:07:27 +0100

    feat : ajouter methode de loadUserByUsername

--- src/main/java/com/kaoutar/Eventify/security/CustomUserDetailsService.java
+++ src/main/java/com/kaoutar/Eventify/security/CustomUserDetailsService.java
@@ -1,4 +1,20 @@
 package com.kaoutar.Eventify.security;
 
-public class CustomUserDetailsService {
+import com.kaoutar.Eventify.model.User;
+import com.kaoutar.Eventify.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+@RequiredArgsConstructor
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User u = userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found: " + username));
+        return new CustomUserDetails(u);
+    }
 }
