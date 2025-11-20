3cfd1966054d026a87fada3aa37ba46dc94bc53b
Kaoutar laajil <129660727+kooutar@users.noreply.github.com>
2025-11-20 14:44:15 +0100

    feat : cree filterChain sans filter les permission  & passwordEncoder

--- src/main/java/com/kaoutar/Eventify/config/SecurityConfig.java
+++ src/main/java/com/kaoutar/Eventify/config/SecurityConfig.java
@@ -1,4 +1,37 @@
 package com.kaoutar.Eventify.config;
 
+import com.kaoutar.Eventify.security.CustomAuthenticationProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableMethodSecurity
 public class SecurityConfig {
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll()
+                );
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
 }
